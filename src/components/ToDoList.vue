06d9b70d990098803faf16396d3c6692aa8443d5
vjenacka <edins613@gmail.com>
2019-12-04 23:28:23 +0100

    added func for adding todos and added template for todo list and single todo

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="container">
     <div class="add-item">
-      <div class="add-icon">
+      <div class="add-icon" @click="addItem">
         <i class="fas fa-plus"></i>
       </div>
-      <form action="#">
-        <input type="text" placeholder="Add a to-do..." />
+      <form @submit.prevent="addItem">
+        <input type="text" placeholder="Add a to-do..." v-model="entry" />
       </form>
       <div class="feature-icon">
-        <i class="far fa-star"></i>
+        <i v-if="!entryFavorite" class="far fa-star" @click="entryFavorite = !entryFavorite"></i>
+        <i v-else class="fas fa-star" @click="entryFavorite = !entryFavorite"></i>
+      </div>
+    </div>
+    <div class="todo-list">
+      <div class="item" v-for="(item,index) in todoList" :key="index">
+        <div class="item-checkbox">
+          <i class="fas fa-square"></i>
+        </div>
+        <div class="item-title">{{item.title}}</div>
+        <div class="feature-icon">
+          <i v-if="!item.favorite" class="far fa-star" @click="item.favorite = !item.favorite"></i>
+          <i v-else class="fas fa-star" @click="item.favorite = !item.favorite"></i>
+        </div>
       </div>
     </div>
   </div>
@@ -16,7 +29,39 @@
 
 <script>
 export default {
-  name: "ToDoList"
+  name: "ToDoList",
+  data: () => {
+    return {
+      entry: "",
+      entryFavorite: false,
+      todoList: []
+    };
+  },
+  methods: {
+    addItem() {
+      if (this.entry !== "") {
+        let date = new Date();
+        let newEntry = {
+          id: date.getTime(),
+          title: this.entry,
+          favorite: this.entryFavorite,
+          complete: false,
+          show: false
+        };
+
+        if (newEntry.favorite) {
+          //push new item on first position
+          this.todoList.splice(0, 0, newEntry);
+        } else {
+          // push new item on last position
+          this.todoList.push(newEntry);
+        }
+      }
+
+      this.entry = "";
+      this.entryFavorite = false;
+    }
+  }
 };
 </script>
 
@@ -25,7 +70,18 @@ export default {
   padding: 10px;
   width: calc(100% - 20px);
   min-height: calc(100% - 20px);
-
+  //feature icon class
+  .feature-icon {
+    grid-column-start: 3;
+    grid-column-end: 3;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    font-size: 2rem;
+    cursor: pointer;
+  }
+  //form for adding to-dos
   .add-item {
     display: grid;
     grid-template-columns: 70px auto 70px;
@@ -63,17 +119,6 @@ export default {
       &:focus {
         outline: none;
       }
-    }
-
-    .feature-icon {
-      grid-column-start: 3;
-      grid-column-end: 3;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      color: #fff;
-      font-size: 2rem;
-      cursor: pointer;
     }
   }
 }
